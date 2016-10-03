c2ba7b92d5cf0601d45b44c57b960bab6dd87ca6
Vicent Gomez <gomezv@uv.es>
2016-10-03 10:18:45 +0200

    Prueba commit

---
+++ SubiendoGit/src/SubiendoGit.java
@@ -0,0 +1,45 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class SubiendoGit extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SubiendoGit frame = new SubiendoGit();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SubiendoGit() {
+		setTitle("SubiendoGit");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblSubiendo = new JLabel("Subiendo");
+		lblSubiendo.setBounds(175, 108, 104, 36);
+		contentPane.add(lblSubiendo);
+	}
+}
