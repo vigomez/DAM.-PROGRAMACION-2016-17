db6b2ac1960fc75491813cbc30898d4e8d6a8b56
Vicent Gomez <gomezv@uv.es>
2017-02-09 12:17:01 +0100

    Conexión BBDD

--- L22UsuariosBBDD/src/Usuarios.java
+++ L22UsuariosBBDD/src/Usuarios.java
@@ -1,23 +1,23 @@
 
 public class Usuarios {
-	static ConexionDB corredoresDB;
-	static modeloUsuarios mCorredor; //Para sacar lista de BBDD
+	static ConexionDB usuariosDB;
+	static modeloUsuarios mUsuario; //Para sacar lista de BBDD
 	
 	public static void main(String[] args) {
 
 		
 		//Generado el objeto
-		corredoresDB=ConexionDB.getInstance("localhost","usuarios","root","");
+		usuariosDB=ConexionDB.getInstance("localhost","usuarios","root","");
 		
 		//Ejemplo para un host externo
 		//corredoresDB=ConexionDB.getInstance("mysql.hostinger.es","u499092340_dam20","u499092340_dam20","12345");
 		
         //Comprobamos si hemos conseguido conectarnos
 
-		if(corredoresDB.connectDB()==true) {
+		if(usuariosDB.connectDB()==true) {
 			System.out.println("CONECTADOS CON EXITO");
-			mCorredor=new modeloUsuarios(corredoresDB.getConexion());
-			mCorredor.getDatosUsuarios();
+			mUsuario=new modeloUsuarios(usuariosDB.getConexion()); //Mostramos todos los elementos de la BBDD
+			mUsuario.getDatosUsuarios();
 			System.out.println("LISTADO DE USUARIOS");
 		}
 		else System.out.println("ERROR EN LA CONEXION");
